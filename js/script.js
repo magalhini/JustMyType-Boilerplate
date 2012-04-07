27156dd8fc9cc32bdaf68a671e3b3d11707a8c6e
Ricardo Magalhaes <magalhini@gmail.com>
2012-04-07 15:05:58 +0100

    Add drop-caps class. Started to work on dynamic grid overlay.

--- js/script.js
+++ js/script.js
@@ -1,52 +1,96 @@
 /*
- * Type Baseline
- *
- * Author: Ricardo Magalhães
- * Version: 0.1
- */
+* Type Baseline & Grid
+*
+* Author: Ricardo Magalhães
+* Version: 0.2
+*/
 
-$(function () {
+function showGrid (cols) {
 
-	showGrid(16);
-	/* Display a vertical rythm grid to confirm the values */
-	function showGrid (size, line) {
+	var $body = $(document.body),
+		nrOfCols = cols;
+		$panel = $('<div></div>'),
+		$grid = $('<div class="grid"></div>')
+			.appendTo($body)
+			.css({
+				'position': 'absolute',
+				'left' : 0,
+				'top' : 0,
+				'width' : '100%',
+				'z-index' : '99'
+			});
 
-		var oldWrap = $('.line-height');
-		if (oldWrap.length) oldWrap.remove();
+	createPanel().appendTo($body);
+	createGrids(nrOfCols);
 
-		this.options = {
-				fontSize: size,
-				lineHeight: line
-		};
+	function createGrids (cols) {
+		var i = 1,
+			$col = "";
 
-		heightLines(this.options);
-
-		function heightLines (options) {
-
-			var fontSize = options.fontSize || 16,
-				lineHeight =  options.lineHeight / options.fontSize || 1.5,
-				unit = fontSize * lineHeight,
-				wrapper = $('<div></div>')
-				.addClass('line-height')
-				.appendTo($(document.body)),
-				rows = ($(document).height() - 25) / unit;
-
-			for (var i = 0; i < rows; i++) {
-
-				var grid = $('<div></div>').css({
-					'height': unit,
-					'width': '100%',
-					'border-bottom': '1px dotted #ccc',
-					'position': 'absolute',
-					'top': unit * i,
-					'content': ''
-				}).appendTo('.line-height');
-			}
-
-			$('.line-height').css('overflow', "hidden");
+		for ( ; i <= cols; i++) {
+			$col = $('<div class="jmt-'+ i +'"></div>');
+			$col.appendTo($grid);
 		}
 	}
-});
+
+	function createPanel() {
+		$panel.addClass('panel')
+			.css({
+				'position' : 'absolute',
+				'width' : '9em',
+				'height' : '2em',
+				'top' : 0,
+				'left' : 0,
+				'background-color' : '#cecece',
+				'border-radius' : '0 5px 5px 0',
+				'z-index' : 999,
+				'box-shadow': '0px 0px 0px 1px rgba(255,255,255,.5) inset',
+				'border' : '1px solid #888'
+		});
+
+		return $panel;
+	}	
+}
+
+/* Display a vertical rythm grid to confirm the values */
+function showBaseline (size, line) {
+
+	var oldWrap = $('.line-height');
+
+	if (oldWrap.length) oldWrap.remove();
+
+	this.options = {
+		fontSize: size,
+		lineHeight: line
+	};
+
+	heightLines(this.options);
+
+	function heightLines (options) {
+
+		var fontSize = options.fontSize || 16,
+			lineHeight =  options.lineHeight / options.fontSize || 1.5,
+			unit = fontSize * lineHeight,
+			wrapper = $('<div></div>')
+			.addClass('line-height')
+			.appendTo($(document.body)),
+			rows = ($(document).height() - 25) / unit;
+
+		for (var i = 0; i < rows; i++) {
+
+			var grid = $('<div></div>').css({
+				'height': unit,
+				'width': '100%',
+				'border-bottom': '1px dotted #ccc',
+				'position': 'absolute',
+				'top': unit * i,
+				'content': ''
+			}).appendTo('.line-height');
+		}
+
+		$('.line-height').css('overflow', "hidden");
+	}
+}
 
 
 
